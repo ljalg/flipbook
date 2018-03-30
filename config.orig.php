8c531bbc376207f33c0a5ab3dddc921c6ac11307
ljalg <ljconsultinginc@gmail.com>
2018-03-30 16:03:10 +0000

    Add config.orig.php file added config.orig.php file

---
+++ config.orig.php
@@ -0,0 +1,15 @@
+<?php
+
+// Database variables
+$servername = "xxx";
+$databasename = "xxx";
+$username = "xxx";
+$password = "xxx";
+
+$phpmyadminURL = "phpmyadmin";
+$server_url = "https://yoursite/yourfolder";
+//System variables
+
+// object property names which should not be displayed.  
+// each one should be in single quotes separated by commas
+$reserved_object_properties = "'Password','xxx'";
